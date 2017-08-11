92896f949000c4e20953a04103411ef91ab02e9e
shobana-narendren13 <30838979+shobana-narendren13@users.noreply.github.com>
2017-08-11 22:38:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ app.get('/', function (req, res) {
 
 
 app.get('/page-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
+  //res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
+  res.send("Page one executed");
+  
+});
+app.get('/page-two', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
+  res.send("Page two executed");
+  
+});
+app.get('/page-three', function (req, res) {
+  //res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
+  res.send("Page three executed");
+  
 });
 
 
