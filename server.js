b6c2ceac8f59087b6ea1c8fa556862d89369eb9c
shobana-narendren13 <30838979+shobana-narendren13@users.noreply.github.com>
2017-08-11 22:23:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/', function (req, res) {
+app.get('/page-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
 });
 
