3503465e97d0d26712a67284c548e94c03f3941b
shobana-narendren13 <30838979+shobana-narendren13@users.noreply.github.com>
2017-08-11 23:26:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,17 @@
-console.log('Loaded!');
+var input_name=document.getElementById('comments');
+var text=input_name.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function()
+{
+    var comments=['comment1','comment2','comment3'];
+    var list ='';
+    for(var i=0; i<comments.length;i++)
+    {
+        list +='<li>+comments[i]+</li>';
+        
+    }
+    var comments_list=getElementById('comments_list');
+    comments_list.innerHTML=list;
+};
+
+
