9b93a4b6ccb949ad4b0ad2cf8370e906487cd736
shobana-narendren13 <30838979+shobana-narendren13@users.noreply.github.com>
2017-08-13 00:29:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,6 +32,6 @@ submit.onclick = function()
 
 var input_name=document.getElementById('comments');
 var comments=input_name.value;
-//request.open('GET','http://imad.hasura.io/submit-name?comments=' + comments,true);
-//request.send(null);
+request.open('GET','http://imad.hasura.io/submit-name?comments=' + comments,true);
+request.send(null);
 
