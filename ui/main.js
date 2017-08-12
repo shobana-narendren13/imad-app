bb2ecc207220f3944998f04151250f852a93e2df
shobana-narendren13 <30838979+shobana-narendren13@users.noreply.github.com>
2017-08-13 00:16:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,17 +2,17 @@
 var submit=document.getElementById('submit_btn');
 submit.onclick = function()
 {
-  //  var request=new XMLHttpRequest();
-//    request.onreadystatechange=function()
-  //  {
-    //    if(request.readyState === XMLHttpRequest.DONE )
-      //  {
-        //    if(request.status === 200)
-          //  {
-            //    var comment=request.responseText;
-              //  comment=JSON.parse(comment);
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+  {
+        if(request.readyState === XMLHttpRequest.DONE )
+       {
+            if(request.status === 200)
+            {
+                var comment=request.responseText;
+                comment=JSON.parse(comment);
            
-    var comment=['comments1','comments2','comments3'];
+   // var comment=['comments1','comments2','comments3'];
     var list ='';
     for(var i=0; i<comment.length;i++)
     {
@@ -22,14 +22,14 @@ submit.onclick = function()
     var ul=document.getElementById('comments_list');
     ul.innerHTML=list;
     
-          //  }
-       // }
+            }
+        }
         
-//};
+};
 };
 
 var input_name=document.getElementById('comments');
 var comments=input_name.value;
-request.open('GET','http://imad.hasura.io/submit-name?comments=' + comment,true);
+request.open('GET','http://imad.hasura.io/submit-name?comments=' + comments,true);
 request.send(null);
 
