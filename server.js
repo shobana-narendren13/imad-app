8cccb2bdf3ecc08cabf5bf18eb5775551293d8dc
shobana-narendren13 <30838979+shobana-narendren13@users.noreply.github.com>
2017-08-18 00:15:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     user:'	shobananarendren13',
     database:'shobananarendren13',
     host:'db.imad.hasura-app.io',
-    port:'5432',
+    port:'80',
     password:process.env.DB_PASSWORD
 };
 
